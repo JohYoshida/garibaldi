0e5393203750422d49b3c9ac352a9a2eb64d01d9
Joh Yoshida <johyoshida@gmail.com>
2018-01-23 22:23:57 -0800

    Refactor and post '/' route

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+// Server requirements
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .get('/new', (req, res) => res.render('new'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// Middleware
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')));
+
+// View engine
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Routes
+app.get('/', (req, res) => res.render('index'))
+app.post('/', (req, res) => {
+  console.log('Title:', req.body.title);
+  console.log('Body:', req.body.body);
+  res.render('index');
+})
+app.get('/new', (req, res) => res.render('new'))
+
+// Start server
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
