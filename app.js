348b63532c3557948a33efe4aed15102ef318c72
Joh Yoshida <johyoshida@gmail.com>
2018-01-24 00:29:10 -0800

    Prettier

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 // Server requirements
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const uuid = require('uuid/v1');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const uuid = require("uuid/v1");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database setup
-const dbconfig = require('./knexfile.js')[process.env.DB_ENV];
-const knex = require('knex')(dbconfig);
+const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
+const knex = require("knex")(dbconfig);
 
 // Middleware
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // View engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Routes
-app.get('/', (req, res) => {
-  knex('articles').then(articles => {
-    res.render('index', {articles});
+app.get("/", (req, res) => {
+  knex("articles").then(articles => {
+    res.render("index", { articles });
   });
 });
-app.post('/', (req, res) => {
-  knex('articles').insert({
-    id: uuid(),
-    title: req.body.title,
-    text: req.body.text
-  }).then(() => res.redirect('/'));
+app.post("/", (req, res) => {
+  knex("articles")
+    .insert({
+      id: uuid(),
+      title: req.body.title,
+      text: req.body.text
+    })
+    .then(() => res.redirect("/"));
 });
-app.get('/new', (req, res) => res.render('new'));
+app.get("/new", (req, res) => res.render("new"));
 
 // Start server
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
