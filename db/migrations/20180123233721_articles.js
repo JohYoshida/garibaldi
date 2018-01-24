9aa0fbf2a44af44b711f188c7c69d17d6b6bfc35
Joh Yoshida <johyoshida@gmail.com>
2018-01-24 00:25:52 -0800

    Articles migration

---
+++ db/migrations/20180123233721_articles.js
@@ -0,0 +1,16 @@
+const uuid = require('uuid/v1');
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('articles', table => {
+      table.string('id');
+      table.string('title');
+      table.string('text');
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTableIfExists('articles')
+  ]);
+};
