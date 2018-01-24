39729e3e39d25b397bb1cfe88eaa7a8c68b9b4be
Joh Yoshida <johyoshida@gmail.com>
2018-01-24 00:31:37 -0800

    Prettier

--- db/migrations/20180123233721_articles.js
+++ db/migrations/20180123233721_articles.js
@@ -1,16 +1,14 @@
-const uuid = require('uuid/v1');
+const uuid = require("uuid/v1");
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('articles', table => {
-      table.string('id');
-      table.string('title');
-      table.string('text');
+    knex.schema.createTable("articles", table => {
+      table.string("id");
+      table.string("title");
+      table.string("text");
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTableIfExists('articles')
-  ]);
+  return Promise.all([knex.schema.dropTableIfExists("articles")]);
 };
--- db/seeds/add_articles.js
+++ db/seeds/add_articles.js
@@ -1,20 +1,21 @@
-const uuid = require('uuid/v1');
+const uuid = require("uuid/v1");
 
 exports.seed = function(knex, Promise) {
   // Delete all existing entries
-  return knex('articles').del()
-    .then(function () {
+  return knex("articles")
+    .del()
+    .then(function() {
       // Insert seed entries
-      return knex('articles').insert([
+      return knex("articles").insert([
         {
           id: uuid(),
-          title: 'Sample Article 1',
-          text: 'This is what an article will look like.'
+          title: "Sample Article 1",
+          text: "This is what an article will look like."
         },
         {
           id: uuid(),
-          title: 'Sample Article 2',
-          text: 'This is what another article will look like.'
+          title: "Sample Article 2",
+          text: "This is what another article will look like."
         }
       ]);
     });
