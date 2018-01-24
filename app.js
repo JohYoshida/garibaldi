bcfbb76a600cb4fa0304e3548acc96e7b43c6489
Joh Yoshida <johyoshida@gmail.com>
2018-01-23 23:13:51 -0800

    On POST '/', save new articles to temporary database

--- app.js
+++ app.js
@@ -6,23 +6,32 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Temporary article database
+const articles = [
+  {title: 'Article Example', text: 'This is what an article looks like'}
+];
+
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 // app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')));
 
 // View engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Routes
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('index', {articles}));
 app.post('/', (req, res) => {
-  console.log('Title:', req.body.title);
-  console.log('Body:', req.body.body);
-  res.render('index');
-})
-app.get('/new', (req, res) => res.render('new'))
+  articles.push({
+    title: req.body.title,
+    text: req.body.text
+  });
+  res.redirect('/');
+});
+app.get('/new', (req, res) => res.render('new'));
 
 // Start server
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
