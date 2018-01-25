d2fb1af11b3e42e0a1a6909430f009ef30758521
Joh Yoshida <johyoshida@gmail.com>
2018-01-24 17:37:04 -0800

    Refactor and GET /article/:id/edit

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const uuid = require("uuid/v1");
 
+// App setup
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database setup
 const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
 const knex = require("knex")(dbconfig);
+
+// Scripts
+// import {getAllArticles} from "lib/knex-queries";
+const getAllArticles = require("./lib/get-all-articles");
+const getOneAricle = require("./lib/get-one-article");
+const postArticle = require("./lib/post-article");
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,28 +28,18 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  knex("articles").then(articles => {
-    res.render("index", { articles });
-  });
+  getAllArticles(res);
 });
 app.post("/", (req, res) => {
-  knex("articles")
-    .insert({
-      id: uuid(),
-      title: req.body.title,
-      text: req.body.text
-    })
-    .then(() => res.redirect("/"));
+  postArticle(req.body, res);
 });
 app.get("/new", (req, res) => res.render("new"));
 app.get("/articles/:id", (req, res) => {
-  knex("articles")
-    .where({ id: req.params.id })
-    .first()
-    .then(article => {
-      res.render("show", { article });
-    });
+  const mode = "show";
+  getOneAricle(req.params.id, res, "show");
 });
-
+app.get("/articles/:id/edit", (req, res) => {
+  getOneAricle(req.params.id, res, "edit");
+})
 // Start server
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
