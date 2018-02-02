dd8289b9938454f750d7634fc90448f7a9d86e64
Joh Yoshida <johyoshida@gmail.com>
2018-02-01 17:39:10 -0800

    Allows addition/removal of header, paragraph, and image components to article form

---
+++ public/scripts/add-article-components.js
@@ -0,0 +1,35 @@
+$(function() {
+  $(".add-header").click(function() {
+    insertHeader();
+  });
+  $(".add-paragraph").click(function() {
+    insertParagraph();
+  });
+  $(".add-image").click(function() {
+    insertImage();
+  });
+  $("form").on("click", "button.remove-component", function() {
+    $(this).parent().remove();
+  });
+});
+
+function insertHeader() {
+  $("<section></section>").insertBefore(".article-submit")
+  .append("<button class='remove-component' type='button'>Remove Header</button>")
+  .append("<input type='text' name='header' placeholder='header'>")
+  .append("<input type='hidden' name='component' value='h'>");
+}
+
+function insertParagraph() {
+  $("<section></section>").insertBefore(".article-submit")
+  .append("<button class='remove-component' type='button'>Remove Paragraph</button>")
+  .append("<textarea name='paragraph' rows='8' cols='80'></textarea>")
+  .append("<input type='hidden' name='component' value='p'>");
+}
+
+function insertImage() {
+  $("<section></section>").insertBefore(".article-submit")
+  .append("<button class='remove-component' type='button'>Remove Image</button>")
+  .append("<input type='text' name='image' placeholder='image'>")
+  .append("<input type='hidden' name='component' value='i'>");
+}
