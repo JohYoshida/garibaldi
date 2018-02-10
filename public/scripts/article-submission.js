e6af5341bc646148be1a9553ff4f5fa49f97970e
Joh Yoshida <johyoshida@gmail.com>
2018-02-10 13:17:21 -0800

    Add comments

--- public/scripts/article-submission.js
+++ public/scripts/article-submission.js
@@ -1,11 +1,15 @@
 $(function() {
+  // Alert panel starts hidden
   $(".alert").hide();
+
+  // Alert user if article is submitted without a title
   $("form.article-form").submit(function(event) {
+    // Make sure alert panel is empty
     $(".alert").empty();
 
-    var formData = new FormData(this);
+    // Get the value of the title field
     var title = $(".article-title").val();
-
+    // Prevent form submission and add a message to the alert panel
     if (!title) {
       event.preventDefault();
       $('.alert').slideDown().append("<p>Title is required!</p>");
