6951abf98ccba3d82686c36872c9f29548f014fb
Joh Yoshida <johyoshida@gmail.com>
2018-02-09 16:29:59 -0800

    Rename 'remove-component' class to 'remove'and append remove button after inputs

--- public/scripts/add-article-components.js
+++ public/scripts/add-article-components.js
@@ -8,7 +8,7 @@ $(function() {
   $(".add-image").click(function() {
     insertImage();
   });
-  $("form").on("click", "button.remove-component", function() {
+  $("form").on("click", "button.remove", function() {
     $(this)
       .parent()
       .remove();
@@ -17,34 +17,26 @@ $(function() {
 
 function insertHeader() {
   $("<section></section>")
-    .insertBefore(".article-submit")
-    .append(
-      "<button class='remove-component' type='button'>Remove Header</button>"
-    )
+    .appendTo("#components")
     .append("<input type='text' name='header' placeholder='header'>")
-    .append("<input type='hidden' name='component' value='h'>");
+    .append("<input type='hidden' name='component' value='h'>")
+    .append("<button class='remove' type='button'>Remove Header</button>");
 }
 
 function insertParagraph() {
   $("<section></section>")
-    .insertBefore(".article-submit")
-    .append(
-      "<button class='remove-component' type='button'>Remove Paragraph</button>"
-    )
+    .appendTo("#components")
     .append("<textarea name='paragraph' rows='8' cols='80'></textarea>")
-    .append("<input type='hidden' name='component' value='p'>");
+    .append("<input type='hidden' name='component' value='p'>")
+    .append("<button class='remove' type='button'>Remove Paragraph</button>");
 }
 
 let imageCount = 1;
 function insertImage() {
   $("<section></section>")
-    .insertBefore(".article-submit")
-    .append(
-      "<button class='remove-component' type='button'>Remove Image</button>"
-    )
-    .append(
-      "<input type='file' name='images' placeholder='image'>"
-    )
-    .append("<input type='hidden' name='component' value='i'>");
+    .appendTo("#components")
+    .append("<input type='file' name='images' placeholder='image'>")
+    .append("<input type='hidden' name='component' value='i'>")
+    .append("<button class='remove' type='button'>Remove Image</button>");
   imageCount++;
 }
