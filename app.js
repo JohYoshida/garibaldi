6ee8eba49593d8ddb63a5abc601ea68827728040
Joh Yoshida <johyoshida@gmail.com>
2018-03-05 17:15:47 -0800

    POST /register

--- app.js
+++ app.js
@@ -59,6 +59,33 @@ app.post("/", upload.any(), (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
+app.post("/register", (req, res) => {
+  // Check for existing users with that username
+  knex("users")
+    .where({ username: req.body.username })
+    .then(users => {
+      if (users.length === 0) {
+        // Add user to database
+        console.log(`Adding user ${req.body.username} to database`);
+        knex("users")
+          .insert({
+            username: req.body.username,
+            password: req.body.password
+          })
+          .then(() => {
+            // Login
+            req.session.user = req.body.username;
+            req.session.isLoggedIn = true;
+            res.redirect("/");
+          });
+      } else {
+        // TODO: error handling
+        console.log("That username is taken!");
+        res.redirect("/");
+      }
+    });
+});
+// Article routes
 app.get("/new", (req, res) => {
   ArticleHelpers.newArticle(res);
 });
