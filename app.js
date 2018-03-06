56b3157adddf3bbefff746832233b89bf87c1619
Joh Yoshida <johyoshida@gmail.com>
2018-03-05 17:10:15 -0800

    Setup body-parser and cookie-session

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+"use strict";
+
 // Server requirements
 const express = require("express");
 const methodOverride = require("method-override");
+const cookieSession = require("cookie-session");
+const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
 
@@ -15,21 +19,31 @@ const knex = require("knex")(dbconfig);
 // Scripts
 const ArticleHelpers = require("./lib/article-helpers");
 
-// Multer setup
+// Middleware
+// Parse multipart/form-data forms
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, `${process.env.UPLOADS}/images/`);
   },
   filename: function(req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 const upload = multer({ dest: `${process.env.UPLOADS}/images/`, storage });
-
-// Middleware
+// Parse application/x-www-form-urlencoded forms
+app.use(bodyParser.urlencoded({ extended: false }));
+// Use static paths
 app.use(express.static(path.join(__dirname, "./public")));
 app.use(express.static(path.join(__dirname, "./dist")));
+// Method Override
 app.use(methodOverride("_method"));
+// Cookie Session
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["secret", "keys"]
+  })
+);
 
 // View engine
 app.set("views", path.join(__dirname, "views"));
@@ -37,7 +51,7 @@ app.set("view engine", "ejs");
 
 // Routes
 app.get("/", (req, res) => {
-  ArticleHelpers.getAllArticles(res);
+  ArticleHelpers.getAllArticles(res, req.session);
 });
 app.post("/", upload.any(), (req, res) => {
   ArticleHelpers.postArticle(req, res);
@@ -57,5 +71,6 @@ app.delete("/articles/:id", (req, res) => {
 app.get("/articles/:id/edit", (req, res) => {
   ArticleHelpers.getOneAricle(req.params.id, res, "edit");
 });
+
 // Start server
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
