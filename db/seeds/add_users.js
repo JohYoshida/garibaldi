bb0614a0f22b93914e20d760863dbba3e03cfe8c
Joh Yoshida <johyoshida@gmail.com>
2018-03-06 00:13:53 -0800

    Add user seed

---
+++ db/seeds/add_users.js
@@ -0,0 +1,16 @@
+const uuid = require("uuid/v1");
+
+exports.seed = function(knex, Promise) {
+  // Delete all existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Insert seed entries
+      return knex("users").insert([
+        {
+          username: "Joh",
+          password: "pass"
+        }
+      ]);
+    });
+};
