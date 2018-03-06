3e8af9c05ad450ef1a063dc10b0d9f48b3482658
Joh Yoshida <johyoshida@gmail.com>
2018-03-05 17:52:59 -0800

    Refactor user routes

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 const dbconfig = require("./knexfile.js")[process.env.DB_ENV];
 const knex = require("knex")(dbconfig);
 
-// Scripts
+// Functions
 const ArticleHelpers = require("./lib/article-helpers");
 
 // Middleware
@@ -56,59 +56,64 @@ app.get("/", (req, res) => {
 app.post("/", upload.any(), (req, res) => {
   ArticleHelpers.postArticle(req, res);
 });
-// User registration routes
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-app.post("/register", (req, res) => {
-  // Check for existing users with that username
-  knex("users")
-    .where({ username: req.body.username })
-    .then(users => {
-      if (users.length === 0) {
-        // Add user to database
-        console.log(`Adding user ${req.body.username} to database`);
-        knex("users")
-          .insert({
-            username: req.body.username,
-            password: req.body.password
-          })
-          .then(() => {
-            // Login
-            req.session.user = req.body.username;
-            req.session.isLoggedIn = true;
-            res.redirect("/");
-          });
-      } else {
-        // TODO: error handling
-        console.log("That username is taken!");
+// Register
+app.route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    // Check for existing users with that username
+    knex("users")
+      .where({ username: req.body.username })
+      .then(users => {
+        if (users.length === 0) {
+          // Add user to database
+          console.log(`Adding user ${req.body.username} to database`);
+          knex("users")
+            .insert({
+              username: req.body.username,
+              // TODO: bcrypt password
+              password: req.body.password
+            })
+            .then(() => {
+              // Login
+              req.session.user = req.body.username;
+              req.session.isLoggedIn = true;
+              res.redirect("/");
+            });
+        } else {
+          // TODO: error handling
+          console.log("That username is taken!");
+          res.redirect("/");
+        }
+      });
+  });
+// Login
+app.route("/login")
+  .get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+    // Lookup user
+    knex("users")
+      .where({ username: req.body.username })
+      .then(users => {
+        if (users.length === 1) {
+          // Login
+          req.session.user = req.body.username;
+          req.session.isLoggedIn = true;
+        } else {
+          // TODO: error handling
+          console.log(`${users.length} users with that name`);
+        }
         res.redirect("/");
-      }
-    });
-});
+      });
+  });
+// Logout
 app.post("/logout", (req, res) => {
   req.session.isLoggedIn = false;
   req.session.user = null;
   res.redirect("/");
-});
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-app.post("/login", (req, res) => {
-  // Lookup user
-  knex("users")
-    .where({ username: req.body.username })
-    .then(users => {
-      if (users.length === 1) {
-        // Login
-        req.session.user = req.body.username;
-        req.session.isLoggedIn = true;
-      } else {
-        // TODO: error handling
-        console.log(`${users.length} users with that name`);
-      }
-      res.redirect("/");
-    });
 });
 // Article routes
 app.get("/new", (req, res) => {
