5a26d0fd1f51c1fa61919f400b1fb731546ea235
Joh Yoshida <johyoshida@gmail.com>
2018-03-08 21:56:57 -0800

    Updated article seeds

--- db/seeds/add_articles.js
+++ db/seeds/add_articles.js
@@ -12,7 +12,7 @@ exports.seed = function(knex, Promise) {
           user_id: "admin",
           title: "A Proper Article",
           text: "This is what an article will look like.",
-          is_private: 0,
+          is_private: 1,
           created_at: new Date(),
           components: JSON.stringify(["h", "p", "h", "p", "i", "p"]),
           headers: JSON.stringify(["Headers", "Images"]),
@@ -35,26 +35,9 @@ exports.seed = function(knex, Promise) {
           paragraphs: JSON.stringify([
             "It's still pretty barebones, but you can now make articles with headers, paragraphs, and images, in any order or quantity you want. Articles can be marked private, so only you can see them.",
             "Here's a picture of a very fat rat.",
-            "Styling! This website looks like an error page. I'm going to fix that when I feel like struggling with CSS. Let me know if you have any input (or stylesheets)!",
-            "I'm also hoping to make it so you can edit an article without having to start from scratch. Might be difficult with the janky system I built, but we'll see.",
-            "Let me know if there's anything else you want me to add!",
+            "Styling! This website still looks pretty lame. If you have any suggestions (or CSS), let me know!",
+            "Let me know if there's any other features you want me to add!",
             "Joh"
-          ]),
-          images: JSON.stringify(["power.jpeg"])
-        },
-        {
-          id: uuid(),
-          user_id: "admin",
-          title: "A Private Article",
-          text: "This is what an article will look like.",
-          is_private: 1,
-          created_at: new Date(),
-          components: JSON.stringify(["h", "p", "h", "p", "i", "p"]),
-          headers: JSON.stringify(["Headers", "Images"]),
-          paragraphs: JSON.stringify([
-            "A proper article should have a header.",
-            "A proper article may have an image.",
-            "The image may be surrounded by text."
           ]),
           images: JSON.stringify(["power.jpeg"])
         }
