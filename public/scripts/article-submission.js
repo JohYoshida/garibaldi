b71e05bdeebd097bb82fe52aee8f2eed3c538dc1
Joh Yoshida <johyoshida@gmail.com>
2018-03-09 00:47:17 -0800

    Refactored alert display

--- public/scripts/article-submission.js
+++ public/scripts/article-submission.js
@@ -4,15 +4,15 @@ $(function() {
 
   // Alert user if article is submitted without a title
   $("form.article-form").submit(function(event) {
-    // Make sure alert panel is empty
-    $(".alert").empty();
+    // Make sure alert panel is hidden
+    $(".alert").hide();
 
     // Get the value of the title field
     var title = $(".article-title").val();
     // Prevent form submission and add a message to the alert panel
     if (!title) {
       event.preventDefault();
-      $('.alert').slideDown().append("<p>Title is required!</p>");
+      $(".alert").slideDown();
     }
   });
 });
